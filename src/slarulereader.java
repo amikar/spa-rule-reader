7e7b8eeb7c73cc85f6f5a99d8448f09e1d7c1531
amikar <amikardivij@gmail.com>
2017-08-12 23:22:00 -0400

    First commit

---
+++ src/slarulereader.java
@@ -0,0 +1,62 @@
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+
+
+public class slarulereader {
+    public static void main(String[] args) throws Exception, JSONException, IOException{
+    	
+    	
+    	String jsonitter;
+    	StringBuilder app = new StringBuilder();
+    	
+    	URL appsname = new URL("http://13.56.90.73:8161/monitoring-services/resources/monitoring/getslarules");
+    	URLConnection hj = appsname.openConnection();
+        BufferedReader ne = new BufferedReader(new InputStreamReader(hj.getInputStream()));
+        while ((jsonitter = ne.readLine()) != null) 
+        	app.append(jsonitter); 
+        	
+     	JSONObject jsonrules = new JSONObject(app.toString());
+     	
+    
+    	
+   		
+    	JSONArray stagesjson = jsonrules.getJSONArray("slaRules");
+    	
+    	System.out.println(stagesjson);
+
+    	
+    	int numberofrules = stagesjson.length();
+    	String stagetype = "";
+    	String servicetype = "";
+    	String desc = "";
+    	for (int i = 0; i < numberofrules; ++i) {
+   			
+     		JSONObject stagenamejson = stagesjson.getJSONObject(i);
+    
+     		stagetype = stagenamejson.getString("metric");
+     		servicetype = stagenamejson.getString("service");
+     		if (stagetype.equals("googleglass.temperature") && servicetype.equals("GOOGLE_GLASS"))
+     		{
+     			desc = stagenamejson.getString("description");
+     		}
+    	}
+    	
+ 		System.out.println(desc);
+ 		
+ 		String[] new22 = desc.split("\\{OR\\}");
+ 		
+ 		System.out.println("First sla condition : " + new22[0]);
+ 		System.out.println("Second sla condition : " + new22[1]);
+
+
+    }
+    
+		
+		
+}
+		
